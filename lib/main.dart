7ff3ef2f35c4f93f76c9d52187d732aba4302b13
Pedro Lucas <pedrodevmob@gmail.com>
2024-10-15 15:48:21 -0300

    Acrescentando firebase no projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+void main() async{
+  /*
+  WidgetsFlutterBinding.ensureInitialized() 
+  garante que o binding do Flutter esteja pronto antes de executar código que dependa de recursos do Flutter, 
+  como a inicialização de plugins ou operações assíncronas. 
+  É essencial antes de runApp() em cenários que exigem configuração prévia.
+   */
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
